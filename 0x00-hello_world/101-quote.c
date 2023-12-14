fa2cef172c972336080b23cdc64bcfcdb05853f4
AMBITIOUSAWC <abagnawclement@gmail.com>
2023-12-14 15:45:12 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#inlude <unistd.h>
+#inlude < unistd.h >
 /**
  * main - this prints exactly the imput on it
  * Return: always 1.
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 			return (1);
 }
