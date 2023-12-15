4bdfaed3cc4d60b2630a91922aabae174cbc9ed2
AMBITIOUSAWC <abagnawclement@gmail.com>
2023-12-14 17:29:31 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
-  *main - This is my printf function
+  *main - this is my printf function
   *Return: always (0)
   */
 int main(void)
-{               printf("with proper grammar, but thecome is a piece of art,\n");
-                                        return (0);
-
+{
+	printf("with proper grammar, but thecome is a piece of art,\n");
+	return (0);
+}
