89863e53fefe0c144a8ed9aac68c69c37b7d4513
AMBITIOUSAWC <abagnawclement@gmail.com>
2023-12-15 14:48:59 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,17 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
-int main(void) {
-    for (int i = 0; i <= 7; i++) {
-        for (int j = i + 1; j <= 8; j++) {
-            for (int k = j + 1; k <= 9; k++) {
-                putchar((i % 10) + '0');
-                putchar((j % 10) + '0');
-                putchar((k % 10) + '0');
-                putchar((i != 7 || j != 8 || k != 9) ? ',' : '\n');
-            }
-        }
-    }
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
 
-    return 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
 }
 
