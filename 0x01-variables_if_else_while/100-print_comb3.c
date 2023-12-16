07685dd88b5faa0517db23af153722f8035857b0
AMBITIOUSAWC <abagnawclement@gmail.com>
2023-12-15 17:59:31 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-/* 
-The main function is the entry point of the program. It returns an integer value and takes no arguments.
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 9; i++)
-	for (j = i + 1; j < 10; j++)
+	for (i = 48; i < 58; i++)
+	for (j = i + 1; j < 58; j++)
 	{
-		putchar(i + '0');
-		putchar(j + '0');
-
-		if (i != 8 || j != 9)
+		putchar(i);
+		putchar(j);
+		if (i != 56 || j != 57)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(44);
+			putchar(32);
 		}
 	}
 	putchar('\n');
