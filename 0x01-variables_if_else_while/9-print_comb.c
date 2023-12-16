2e721be3f5aa855403e1f42a6976ffc205172074
AMBITIOUSAWC <abagnawclement@gmail.com>
2023-12-15 18:33:05 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 
-/* 
-The main function is the entry point of the program. It returns an integer value and takes no arguments.
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/* Declare an integer variable i */
 	int i;
 
-	/* Loop from 0 to 9 */
 	for (i = 0; i < 10; i++)
-		/* Print the current number */
+	{
 		putchar(i + '0');
-	/* If the current number is not 9, print a comma and a space */
-	if (i != 9)
-		putchar(',');
-	putchar(' ');
-	/* Print a newline character after the loop */
+
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
 	putchar('\n');
-	/* Return 0 to indicate successful execution */
+
 	return (0);
 }
-}
+
