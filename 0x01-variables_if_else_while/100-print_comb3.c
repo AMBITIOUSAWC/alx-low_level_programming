cfa2d7567ee681b425c36ed12ff163ee3aabc346
AMBITIOUSAWC <abagnawclement@gmail.com>
2023-12-15 17:25:16 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
+/* 
+The main function is the entry point of the program. It returns an integer value and takes no arguments.
+*/
 int main(void)
 {
-	int tens, ones;
+	int i, j;
 
-	for (tens = 0; tens < 9; tens++)
-		for (ones = tens + 1; ones < 10; ones++)
-			putchar(tens + '0'), putchar(ones + '0'), putchar(tens < 8 || ones < 9 ? ',' : ' '), putchar(tens < 8 || ones < 9 ? ' ' : '\n');
+	for (i = 0; i < 9; i++)
+	for (j = i + 1; j < 10; j++)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
 
-	return 0;
+		if (i != 8 || j != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
+}
