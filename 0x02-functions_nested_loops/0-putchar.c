79fc4a444d93a9e1b8932a69099eaee6e704ba08
AMBITIOUSAWC <abagnawclement@gmail.com>
2023-12-19 06:15:14 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,38 @@
+/* main.h file */
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
+/* _putchar.c file */
+#include "main.h"
+
+int _putchar(char c)
+{
+    /* Your implementation of _putchar comes here */
+}
+
+/* 0-putchar.c file */
+#include "main.h"
+
+/**
+ * main - prints "_putchar", followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return (0);
+}
