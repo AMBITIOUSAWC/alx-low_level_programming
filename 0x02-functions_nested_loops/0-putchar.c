7564e0d5c455e30470c8a6eb694cb616b460278f
AMBITIOUSAWC <abagnawclement@gmail.com>
2023-12-19 06:29:29 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,30 +9,39 @@ int _putchar(char c);
 /* _putchar.c file */
 #include "main.h"
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
-    /* Your implementation of _putchar comes here */
+	/* Your implementation of _putchar comes here */
+	return (0); /* replace with your code */
 }
 
 /* 0-putchar.c file */
 #include "main.h"
 
 /**
- * main - prints "_putchar", followed by a new line
+ * main - Entry point, prints "_putchar", followed by a new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-    return (0);
+	return (0);
 }
+
