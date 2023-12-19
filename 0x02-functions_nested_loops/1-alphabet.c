fd24ab78d5a583f7073d484e5b9dbb1f117d70b2
AMBITIOUSAWC <abagnawclement@gmail.com>
2023-12-19 07:14:29 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,8 @@
-/* main.h file */
-#ifndef MAIN_H
-#define MAIN_H
+/* 1-alphabet.c file */
+#include <unistd.h>
 
-void print_alphabet(void);
 int _putchar(char c);
 
-#endif /* MAIN_H */
-
-/* 1-alphabet.c file */
-#include "main.h"
-
-/**
- * print_alphabet - prints the alphabet in lowercase followed by a new line
- */
 void print_alphabet(void)
 {
 	char letter;
