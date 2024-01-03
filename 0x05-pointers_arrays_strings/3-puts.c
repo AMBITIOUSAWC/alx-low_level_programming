89c4023950456f5341d48d6ba6bf1dd164e76735
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-01-03 14:10:25 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/* Declaration of _putchar */
+int _putchar(char c);
+
+/**
+ * _puts - writes a string to stdout
+ * @str: The string to print
+ *
+ * Return: void
+ */
+void _puts(const char *str)
+{
+    while (*str != '\0')
+    {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n');
+}
+
