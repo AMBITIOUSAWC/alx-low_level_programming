d1895b1840131c9fb9eb200f64ea113dc60f3ba7
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-01-04 19:02:53 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,22 +1,32 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * main - check the code
+/*
+ * The main function is the entry point of the program.
+ * It initializes variables, performs operations, and prints the result.
+ * In this example, it sets a value in the array 'a', creates a pointer 'p'
+ * pointing to the variable 'n', and updates the value at (p + 5) to 98.
+ * Finally, it prints the value of a[2].
  *
- * Return: Always 0.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+int n;
+int a[5];
+int *p;
 
-	a[2] = 1024;
-	p = &a[2]; /* Point to a valid memory location within the array */
-	*(p + 2) = 98; /* Now this is valid, as it's within the array */
-	printf("a[2] = %d\n", a[2]);
-	printf("a[4] = %d\n", a[4]); /* This will print 98 */
-	return (0);
+a[2] = 1024;
+p = &n;
+/*
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+ */
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
-
