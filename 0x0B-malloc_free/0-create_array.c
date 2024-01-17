aa2a3d12339e682c713c6b0d83135549c60ee236
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-01-17 05:39:51 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 
 /**
  * create_array - creates an array of chars,
- * and initializes it with a specific char.
+ *                and initializes it with a specific char.
  * @size: the size of the array
  * @c: initial value
  *
@@ -11,18 +11,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    unsigned int i;
+char *array;
+unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+if (size == 0)
+return (NULL);
 
-    array = (char *)malloc(sizeof(char) * size);
-    if (array == NULL)
-        return (NULL);
+array = (char *)malloc(sizeof(char) * size);
 
-    for (i = 0; i < size; i++)
-        array[i] = c;
+if (array == NULL)
+return (NULL);
 
-    return (array);
+for (i = 0; i < size; i++)
+array[i] = c;
+
+return (array);
 }
