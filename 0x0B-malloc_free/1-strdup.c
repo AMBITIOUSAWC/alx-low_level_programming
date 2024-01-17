321caf4417c5360fb1c5826f404d513f01ab6587
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-01-17 00:46:28 -0800

    all files

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * @str: string to duplicate
+ * Return: pointer to duplicated string on success, NULL on error
+ */
+char *_strdup(char *str)
+{
+	char *duplicate_str;
+	int i, len = 0;
+
+	if (str == NULL) /* validate str input */
+		return (NULL);
+
+	while (*(str + len))
+		len++;
+	duplicate_str = malloc(sizeof(char) * (len + 1)); /* allocate memory */
+
+	if (duplicate_str == NULL) /* validate memory */
+		return (NULL);
+
+	for (i = 0; i < len; i++) /* copy the string */
+		*(duplicate_str + i) = *(str + i);
+
+	*(duplicate_str + i) = '\0'; /* null terminate the string */
+
+	return (duplicate_str);
+}
+
+/**
+ * main - main function
+ * Return: 0 on success, 1 on failure
+ */
+int main(void)
+{
+	char *buffer;
+
+	buffer = _strdup("ALX SE");
+	if (buffer == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", buffer);
+	free(buffer);
+	return (0);
+}
