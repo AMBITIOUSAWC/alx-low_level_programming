e76a4d975a1e3e73c300f995fe3023c3939fd6ac
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-01-17 12:35:04 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,88 +1,48 @@
 #include "main.h"
-
 #include <stdlib.h>
 
-#include <stdio.h>
-
-
 /**
-
-* str_concat - concatenates two strings.
-
-* @s1: first string
-
-* @s2: second string
-
-*
-
-* Return: a pointer to a newly allocated space in memory which
-
-* contains the contents of s1, followed by the contents of s2,
-
-* and null terminated. NULL on failure
-
-*/
-
+ * str_concat - concatenates two strings.
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: a pointer to the concatenated string,
+ *         or NULL on failure
+ *
+ * Description:
+ * This function concatenates two strings. It allocates memory
+ * for the new string and copies the contents of s1, followed by
+ * the contents of s2, and null-terminates the result.
+ * If either s1 or s2 is NULL, treat it as an empty string.
+ */
 char *str_concat(char *s1, char *s2)
-
 {
+char *concatenated;
+int len_s1 = 0, len_s2 = 0, i, j;
 
-  int i, j, len1, len2, len;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-  char *result;
+while (s1[len_s1] != '\0')
+len_s1++;
 
+while (s2[len_s2] != '\0')
+len_s2++;
 
-  len1 = len2 = 0;
+concatenated = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 
+if (concatenated == NULL)
+return (NULL);
 
-  if (s1 != NULL)
+for (i = 0; i < len_s1; i++)
+concatenated[i] = s1[i];
 
-  {
+for (j = 0; j < len_s2; j++)
+concatenated[i + j] = s2[j];
 
-     i = 0;
+concatenated[i + j] = '\0';
 
-     while (s1[i++] != '\0')
-
-        len1++;
-
-  }
-
-
-  if (s2 != NULL)
-
-  {
-
-     i = 0;
-
-     while (s2[i++] != '\0')
-
-        len2++;
-
-  }
-
-
-  len = len1 + len2;
-
-  result = (char *)malloc(sizeof(char) * (len + 1));
-
-  if (result == NULL)
-
-     return (NULL);
-
-
-  for (i = 0; i < len1; i++)
-
-     result[i] = s1[i];
-
-  for (j = 0; j < len2; j++, i++)
-
-     result[i] = s2[j];
-
-  result[len] = '\0';
-
-
-  return (result);
-
+return (concatenated);
 }
-
-
