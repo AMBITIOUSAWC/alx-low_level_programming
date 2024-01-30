67fdf6f48eb5a3effd4e755afe58651de26189bd
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-01-30 12:01:39 -0800

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,48 +1,37 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 
-#ifndef PRINT_STRINGS_H
-#define PRINT_STRINGS_H
-
-/* Declare the print_strings function */
-void print_strings(const char *separator, const unsigned int num_strings, ...);
-
-#endif /* PRINT_STRINGS_H */
-
+/**
+ * print_strings - prints strings followed by a new line
+ * @separator: separator between strings
+ * @num_strings: number of strings
+ * Return: void
+ */
 void print_strings(const char *separator, const unsigned int num_strings, ...)
 {
-	unsigned int i;
-	va_list args;
-	char *str;
+va_list args;
+unsigned int i;
+char *str;
 
-	va_start(args, num_strings);
+va_start(args, num_strings);
 
-	if (separator == NULL)
-		separator = "";
+if (separator == NULL)
+separator = "";
 
-	for (i = 0; i < num_strings; i++)
-	{
-		str = va_arg(args, char *);
-
-		if (str == NULL)
-			printf("(nil)");
-		else
-			printf("%s", str);
-
-		if (i < num_strings - 1)
-			printf("%s", separator);
-	}
-	printf("\n");
-
-	va_end(args);
-}
-
-int main(void)
-
+for (i = 0; i < num_strings; i++)
 {
-	/* Test the print_strings function */
-	print_strings(", ", 3, "Hello", "World", "!");
-	return 0;
+str = va_arg(args, char *);
 
+if (str == NULL)
+printf("(nil)");
+else
+printf("%s", str);
+
+if (i < num_strings - 1)
+printf("%s", separator);
 }
+printf("\n");
 
+va_end(args);
+}
