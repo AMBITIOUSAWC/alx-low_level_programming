9dd7ddc2c667d58fc79ae58abed0b8f72536b33d
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-01-30 10:42:15 -0800

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,12 +4,11 @@
  * print_numbers - prints numbers followed by a new line
  * @separator: separator between two numbers
  * @n: number of parameters
- * Return: Always 0
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-unsigned int i;
 va_list ap;
+unsigned int i;
 
 va_start(ap, n);
 
